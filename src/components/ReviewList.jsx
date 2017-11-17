751cc706daee85beff5e33ca97cdcb217fbd7ed5
Kai Chen <kaiche2@cisco.com>
2017-11-17 15:07:20 -0800

    Added Links to ReviewList component, fixed styling for ReviewForm

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link }  from 'react-router-dom';
 
 import '../styles/ReviewList.css'
 import ProfileImage from '../assets/ariana-grande.jpg';
@@ -12,28 +13,30 @@ const ReviewList = () => (
           <h3>Ariana Grande</h3>
           <p>Member since 2017 | 5 Reviews</p>
         </div>
-        {/*TODO: Rewrite into its own component */}
+        {/*TODO: Rewrite into its own List component */}
         <div className="ReviewList">
           {/* Card One */}
+          <Link to="/review" className="ReviewListLink">
+            <div className="ReviewItem mdc-card">
+              <section className="ReviewHeader mdc-card__primary">
+                <h1 className="mdc-card__title mdc-card__title--large">Movie Title</h1>
+                <h2 className="mdc-card__subtitle">Published 11/01/2017</h2>
+              </section>
+              <section className="ReviewText mdc-card__supporting-text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                commodo consequat.
+              </section>
+            </div>
+          </Link>
+          {/* Card Two */}
           <div className="ReviewItem mdc-card">
             <section className="ReviewHeader mdc-card__primary">
               <h1 className="mdc-card__title mdc-card__title--large">Movie Title</h1>
               <h2 className="mdc-card__subtitle">Published 11/01/2017</h2>
             </section>
             <section className="ReviewText mdc-card__supporting-text">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-              commodo consequat.
-            </section>
-          </div>
-          {/* Card Two */}
-          <div className="ReviewItem mdc-card">
-            <section className="mdc-card__primary">
-              <h1 className="mdc-card__title mdc-card__title--large">Movie Title</h1>
-              <h2 className="mdc-card__subtitle">Published 11/01/2017</h2>
-            </section>
-            <section className="mdc-card__supporting-text">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
               veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
