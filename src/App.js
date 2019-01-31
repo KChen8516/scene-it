209d1c299798cec93754b497c0e69acbff2b9efc
Kai Chen <kchen2@atlassian.com>
2019-01-30 18:20:40 -0800

    Updated Home backgrounds, updated @glidejs to @latest

--- src/App.js
+++ src/App.js
@@ -1,108 +1,106 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
 // Firebase setup
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import setAuthToken from './utils/setAuthToken';
+import firebase from "firebase/app";
+import "firebase/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 // Module Imports
-import { ReviewForm, Review, ReviewList, ReviewEdit } from './review';
-import { Navbar } from './navbar';
-import { SideDrawer } from './sidedrawer';
-import Info from './info/Info';
-import { MoviesHome } from './movies';
+import { ReviewForm, Review, ReviewList, ReviewEdit } from "./review";
+import { Navbar } from "./navbar";
+import { SideDrawer } from "./sidedrawer";
+import Info from "./info/Info";
+import { MoviesHome } from "./movies";
 
 // Containers
-import PageNotFound from './components/PageNotFound';
-import ErrorBoundary from './components/ErrorBoundary';
-import PrivateRoute from './components/PrivateRoute';
-import Home from './home/HomeContainer';
+import PageNotFound from "./components/PageNotFound";
+import ErrorBoundary from "./components/ErrorBoundary";
+import PrivateRoute from "./components/PrivateRoute";
+import Home from "./home/HomeContainer";
 
 // CSS and assets
-import '@material/layout-grid/dist/mdc.layout-grid.min.css';
-import '@material/typography/dist/mdc.typography.min.css';
-import '@material/elevation/dist/mdc.elevation.min.css';
-import './App.css';
-import { MDCTemporaryDrawer } from '@material/drawer/dist/mdc.drawer.min';
+import "@material/layout-grid/dist/mdc.layout-grid.min.css";
+import "@material/typography/dist/mdc.typography.min.css";
+import "@material/elevation/dist/mdc.elevation.min.css";
+import "./App.css";
+import { MDCTemporaryDrawer } from "@material/drawer/dist/mdc.drawer.min";
 
 class App extends Component {
-  /**
+	/**
    * React only provides 'this' context to lifecycle methods such as the constructor.
    */
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    // console.log('App props', this.props);
+		// console.log('App props', this.props);
 
-    // Check for token
-    if (localStorage.googleToken) {
-      console.log('Local token found');
-      // Set auth token header
-      setAuthToken(localStorage.googleToken);
-      // Grab the user from firebase
-      firebase.auth().onAuthStateChanged(res => {
-        if (res) {
-          const { displayName, email, photoURL, uid } = res.providerData[0];
-          const user = {
-            displayName: displayName,
-            email: email,
-            image: photoURL,
-            googleID: uid
-          };
-          this.props.login(user);
-        }
-      });
-    }
-  }
+		// Check for token
+		if (localStorage.googleToken) {
+			console.log("Local token found");
+			// Set auth token header
+			setAuthToken(localStorage.googleToken);
+			// Grab the user from firebase
+			firebase.auth().onAuthStateChanged((res) => {
+				if (res) {
+					const { displayName, email, photoURL, uid } = res.providerData[0];
+					const user = {
+						displayName: displayName,
+						email: email,
+						image: photoURL,
+						googleID: uid,
+					};
+					this.props.login(user);
+				}
+			});
+		}
+	}
 
-  componentDidMount() {
-    this.drawer = new MDCTemporaryDrawer(
-      document.querySelector('.mdc-drawer--temporary')
-    );
-    this.props.history.listen((location, action) => (this.drawer.open = false));
-  }
+	componentDidMount() {
+		this.drawer = new MDCTemporaryDrawer(document.querySelector(".mdc-drawer--temporary"));
+		this.props.history.listen((location, action) => (this.drawer.open = false));
+	}
 
-  componentWillUnmount() {
-    // console.log('App component unmounting...');
-  }
+	componentWillUnmount() {
+		// console.log('App component unmounting...');
+	}
 
-  componentWillReceiveProps(nextProps) {
-    // console.log(nextProps);
-  }
+	componentWillReceiveProps(nextProps) {
+		// console.log(nextProps);
+	}
 
-  render() {
-    const getClassNames = () => {
-      if (this.props.location.pathname !== '/') {
-        return 'App-Content mdc-top-app-bar--fixed-adjust';
-      } else {
-        return 'App-Content';
-      }
-    };
+	render() {
+		const getClassNames = () => {
+			if (this.props.location.pathname !== "/") {
+				return "App-Content mdc-top-app-bar--fixed-adjust";
+			} else {
+				return "App-Content";
+			}
+		};
 
-    return (
-      <div className="App">
-        <Navbar routes={this.props} />
-        <SideDrawer history={this.props.history} />
+		return (
+			<div className="App">
+				<Navbar routes={this.props} />
+				<SideDrawer history={this.props.history} />
 
-        <ErrorBoundary>
-          <div className={getClassNames()}>
-            <Switch>
-              <PrivateRoute path="/review/edit/:id" component={ReviewEdit} />
-              <PrivateRoute exact path="/reviewlist" component={ReviewList} />
-              <PrivateRoute exact path="/reviewform" component={ReviewForm} />
-              <Route exact path="/" component={Home} />
-              <Route path="/movies" component={MoviesHome} />
-              <Route path="/info" component={Info} />
-              <Route path="/review/:id" component={Review} />
-              <Route component={PageNotFound} />
-            </Switch>
-          </div>
-        </ErrorBoundary>
+				<ErrorBoundary>
+					<div className={getClassNames()}>
+						<Switch>
+							<PrivateRoute path="/review/edit/:id" component={ReviewEdit} />
+							<PrivateRoute exact path="/reviewlist" component={ReviewList} />
+							<PrivateRoute exact path="/reviewform" component={ReviewForm} />
+							<Route exact path="/" component={Home} />
+							<Route path="/movies" component={MoviesHome} />
+							<Route path="/info" component={Info} />
+							<Route path="/review/:id" component={Review} />
+							<Route component={PageNotFound} />
+						</Switch>
+					</div>
+				</ErrorBoundary>
 
-        <div className="App-Footer" />
-      </div>
-    );
-  }
+				<div className="App-Footer" />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/home/HomeContainer.jsx
+++ src/home/HomeContainer.jsx
@@ -1,56 +1,54 @@
-import React, { Component } from 'react'
-import Glide from '@glidejs/glide';
-import './Home.css';
-import '@glidejs/glide/dist/css/glide.core.min.css';
+import React, { Component } from "react";
+import Glide from "@glidejs/glide";
+import "./Home.css";
+import "@glidejs/glide/dist/css/glide.core.min.css";
 
 class Home extends Component {
-    componentDidMount() {
-        // PR pending for preventing sliding at the start and end
-        // https://github.com/glidejs/glide/issues/219
-        new Glide('.glide', {
-            type: 'carousel',
-            gap: 0
-        }).mount();
-    }
+	componentDidMount() {
+		// PR pending for preventing sliding at the start and end
+		// https://github.com/glidejs/glide/issues/219
+		new Glide(".glide", {
+			type: "carousel",
+			gap: 0,
+		}).mount();
+	}
 
-    render() {
-        return (
-            <div className="glide">
-                <div className="glide__track" data-glide-el="track">
-                    <ul className="glide__slides" style={{margin: 0}}>
-                        <li className="glide__slide">
-                            <div className="HomePage-1">
-                                <div className="mdc-layout-grid Callout">
-                                    <div className="mdc-layout-grid__inner">
-                                        <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-                                        <p className="Date">September 20, 2017</p>
-                                        <span className="CalloutHeadline">
-                                            Scene It, review and share your favorite movies with friends.
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li className="glide__slide">
-                            <div className="HomePage-2">
-                                <div className="mdc-layout-grid Callout">
-                                    <div className="mdc-layout-grid__inner">
-                                        <div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
-                                        <p className="Date">September 20, 2017</p>
-                                        <span className="CalloutHeadline">
-                                            Write your own review now!
-                                        </span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="glide">
+				<div className="glide__track" data-glide-el="track">
+					<ul className="glide__slides" style={{ margin: 0 }}>
+						<li className="glide__slide">
+							<div className="Screen Screen--Image-One">
+								<div className="mdc-layout-grid Callout">
+									<div className="mdc-layout-grid__inner">
+										<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+											<p className="Date">September 20, 2017</p>
+											<span className="Callout__Headline">
+												Scene It, review and share your favorite movies with friends.
+											</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li className="glide__slide">
+							<div className="Screen Screen--Image-Two">
+								<div className="mdc-layout-grid Callout">
+									<div className="mdc-layout-grid__inner">
+										<div className="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
+											<p className="Date">September 20, 2017</p>
+											<span className="Callout__Headline">Write your own review now!</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Home;
